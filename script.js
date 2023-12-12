7416b7c608a2083eb8b59354fd3afaa6192bd11f
Caio Braga <caiotabraga@hotmail.com>
2023-12-12 11:46:31 -0300

    scroll section

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-const menuLinks = document.querySelectorAll('.menu-desktop a[href^="#"]');
+document.addEventListener("DOMContentLoaded", function() {
+    // Adiciona o evento de clique para todos os links do menu
+    var links = document.querySelectorAll("nav a");
+    links.forEach(function(link) {
+        link.addEventListener("click", scrollSmooth);
+    });
 
-console.log(menuLinks)
-
-menuLinks.forEach((link) => {
-    link.addEventListener("click", () => console.log("Clicked!"));
+    // Função para rolar suavemente para a seção correspondente
+    function scrollSmooth(e) {
+        e.preventDefault();
+        var targetId = this.getAttribute("href").substring(1);
+        var targetElement = document.getElementById(targetId);
+        targetElement.scrollIntoView({
+            behavior: "smooth"
+        });
+    }
 });
-// function scrollToSection(event) {
-//     event.preventDefault();
-//     const element = event.target;
-//     const id = element.getAttribute("href");
-//     const section = document.querySelector(id);
-//     console.log(section.offsetTop)
-// }
-
-// menuLinks.forEach((link) => {
-//     link.addEventListener("click", scrollToSection);
-// })
